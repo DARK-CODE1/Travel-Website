55154534d07196245bfe512b0d9652eb06259120
DARK-CODE1 <hashemy606@gmail.com>
2024-10-15 02:29:05 +0330

    create reservation section

--- src/components/AdvanceSearch/AdvanceSearch.jsx
+++ src/components/AdvanceSearch/AdvanceSearch.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import './search.css';
+import CustomDropdown from '../CustomDropdown/CustomDropdown';
 
 const AdvanceSearch = () => {
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
   return (
-    <div>AdvanceSearch</div>
+    <section className="box-search-advance">
+      <Container>
+        <Row>
+          <Col md={12} xs={12} >
+            <div className="search-box">
+              <div className="search-item">
+                <CustomDropdown label='location'
+                  options={[
+                    "USA, Turkish",
+                    "Tokyo, Japan",
+                    "Sydney, Australia",
+                    "Melbourne, Australia",
+                    "Paris, France",
+                  ]} />
+              </div>
+              <div className="search-item item-search-2">
+                <label className="item-search-label"> Check In </label>
+                <DatePicker selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                  selectsStart startDate={startDate}
+                  endDate={endDate}
+                  dateFormat="dd, MMMM, yyyy"
+                />
+              </div>
+              <div className="search-item item-search-2">
+                <label className="item-search-label"> Check Out </label>
+                <DatePicker
+                  selected={endDate}
+                  onChange={(date) => setEndDate(date)}
+                  selectsEnd
+                  startDate={endDate}
+                  endDate={startDate}
+                  dateFormat="dd, MMMM, yyyy"
+                />
+              </div>
+              <div className="search-item">
+                <CustomDropdown
+                  label="Guest"
+                  // onSelect={selectedGuest}
+                  options={[
+                    "2 adults, 1 children",
+                    "	2 adults, 1 children",
+                    "2 adults, 3 children",
+                  ]} />
+              </div>
+              <div className="search-item">
+
+              </div>
+
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
--- src/components/CustomDropdown/CustomDropdown.jsx
+++ src/components/CustomDropdown/CustomDropdown.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Dropdown, Form } from 'react-bootstrap';
+import './customdropdown.css';
 
-const CustomDropdown = () => {
+const CustomDropdown = ({ label, options }) => {
+  const [value, setValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleClick = (selectedVal) => {
+    setSelectedValue(selectedVal)
+  }
   return (
-    <div>CustomDropdown</div>
+    <>
+      <label className="item-search-lable">{label}</label>
+      <Dropdown className='dropdown-custom' onSelect={handleClick}>
+        <Dropdown.Toggle id="dropdown-custom-components">
+          {selectedValue ? selectedValue : label}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Form.Control
+            autoFocus
+            className="mx-3 my-2 w-auto"
+            placeholder="Search..."
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+          />
+          <ul className="list-unstyled">
+            {options.filter(
+              (options) =>
+                !value || options.toLowerCase().startsWith(value.toLowerCase()),
+            ).map((option, index) => (
+              <Dropdown.Item eventKey={option} key={index}>
+                {option}
+              </Dropdown.Item>
+            ))
+            }
+          </ul>
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
   )
 }
 
