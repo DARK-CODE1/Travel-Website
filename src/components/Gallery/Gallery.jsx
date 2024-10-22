d93c13852bc553fb1dd53f03519b7e6dcfec29b8
DARK-CODE1 <hashemy606@gmail.com>
2024-10-22 18:07:57 +0330

    complete call us  section

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,44 @@
-import React from 'react'
+import React from 'react';
+import GalleryImg1 from "../../assets/images/gallery/g1.jpg";
+import GalleryImg2 from "../../assets/images/gallery/g2.jpg";
+import GalleryImg3 from "../../assets/images/gallery/g3.jpg";
+import GalleryImg4 from "../../assets/images/gallery/g4.jpg";
+import GalleryImg5 from "../../assets/images/gallery/g5.jpg";
+import GalleryImg6 from "../../assets/images/gallery/g6.jpg";
+import GalleryImg7 from "../../assets/images/gallery/g7.jpg";
 
 const Gallery = () => {
+
+  var images = [
+    {
+      src: GalleryImg1,
+      desc: "Person wearing shoes",
+      sub: "Gift Habeshaw"
+    },
+    {
+      src: GalleryImg3,
+      desc: "Blonde woman wearing sunglasses smiling at the camera ",
+      sub: "Dmitriy Frantsev"
+    },
+    {
+      src: GalleryImg6,
+      sub: "Harry Cunningham"
+    },
+    {
+      src: GalleryImg4,
+      desc: "Jaipur , Rajasthan India",
+      sub: "Liam Baldock"
+    },
+    {
+      src: GalleryImg7,
+      sub: "Verne Ho"
+    },
+    {
+      src: GalleryImg6,
+      desc: "Rann of kutch , India",
+      sub: "Hari Nandakumar"
+    },
+  ];
   return (
     <div>Gallery</div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,6 +9,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './home.css';
 import Populars from '../../components/Populars/Populars';
+import Gallery from '../../components/Gallery/Gallery';
 
 const Home = () => {
   var settings = {
@@ -96,7 +97,45 @@ const Home = () => {
         </Container>
       </section>
       <Populars />
-      
+      <section className="call_us">
+        <Container>
+          <Row className='align-items-center'>
+            <Col md={8}>
+              <h5 className="title">CALL TO ACTION</h5>
+              <h2 className="heading">READY FOR UNFORGATABLE TRAVEL. REMEMBER US!</h2>
+              <p className="text">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s,{" "}
+              </p>
+            </Col>
+            <Col md={4} className='text-center mt-3 my-0'>
+              <a href="tel:1324569875" className="secondary_btn bounce" rel="no">
+                {" "}
+                Contact Us !
+              </a>
+            </Col>
+          </Row>
+        </Container>
+        <div className="overlay"></div>
+      </section>
+
+      <section className="tours_section slick-slider">
+        <Container>
+          <Row>
+            <Col md='12'>
+              <div className="main_heading">
+                <h1> Photo Gallery </h1>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Gallery />
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </>
   )
 }
