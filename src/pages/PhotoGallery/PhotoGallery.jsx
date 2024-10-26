62b5316a8caf1aa784b367a7a80f38bb8ed0683a
DARK-CODE1 <hashemy606@gmail.com>
2024-10-26 21:26:59 +0330

    complete gallery page

--- src/pages/PhotoGallery/PhotoGallery.jsx
+++ src/pages/PhotoGallery/PhotoGallery.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
+import { Col, Container, Row } from 'react-bootstrap';
+import Gallery from '../../components/Gallery/Gallery';
 
 const PhotoGallery = () => {
   return (
-    <div>PhotoGallery</div>
+    <>
+      <Breadcrumbs title='PhotoGallery' pageName='PhotoGallery' />
+      <section className="py-5" style={{overflow:'hidden'}}>
+        <Container>
+          <Row>
+            <Col>
+              <Gallery />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   )
 }
 
